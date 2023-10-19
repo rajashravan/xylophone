78e788db1cd98565cc04e9efa379d228b9fd4220
Raja Shravan <raja@banter.shopping>
2023-10-19 14:44:59 -0400

    init

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
+    func playKey(key: String) {
+        guard let path = Bundle.main.path(forResource: key, ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("start")
     }
-
-
     
-
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.layer.opacity /= 2
+        let title: String = sender.currentTitle!
+        
+        playKey(key: title)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+            sender.layer.opacity *= 2
+        }
+    }
+    
+    
 }
 
